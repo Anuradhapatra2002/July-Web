4ddf002795915f184d6a1f385dbc9372274d024c
Anuradhapatra2002 <patraanuradha09@gmail.com>
2025-07-27 07:35:52 +0530

    hosting in js

---
+++ day16/hosting in js.js
@@ -0,0 +1,13 @@
+console.log(useName);// before the initialization var will be executed but undefined // memory creatrion 
+
+var useName="abhisek"
+console.log(useName);
+
+//console.log(lastName);//can not acces before initialization // value unavailable // 
+
+//const lastName ="abhisek das"//globaly access
+sayHii()
+function  sayHii(){
+    console.log(" hii hello");
+}
+    
