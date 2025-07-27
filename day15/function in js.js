2b027355934b29052c8e1b004f52f03b26b56de4
Anuradhapatra2002 <patraanuradha09@gmail.com>
2025-07-27 06:54:10 +0530

    execution context in js

---
+++ day15/function in js.js
@@ -0,0 +1,21 @@
+debugger//stop the program at code execution phase
+console.log("program started");
+hello()
+
+//function declaration 
+//syntax =>funtion<function name >(List of para){}
+function hello(){
+    console.log("hello everyone");
+    
+}console.log("program ended");
+function addTwoNumber(x , y=100) {
+    console.log("x value is : " , x);
+    console.log("y value is : " , y);
+    
+    
+    console.log(x+y);
+}
+addTwoNumber(100  ,200)
+
+
+
