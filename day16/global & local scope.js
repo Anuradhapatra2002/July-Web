ab2756dc8b9a3153c15c96333c9422d93d73b86b
Anuradhapatra2002 <patraanuradha09@gmail.com>
2025-07-27 09:30:49 +0530

    local & global

---
+++ day16/global & local scope.js
@@ -0,0 +1,13 @@
+let x=10
+const y =20
+var a=120
+
+function f1(){
+let  b=220;
+    console.log("with in function a " +a);
+     console.log("with in function b" +b);
+     
+}
+console.log("outside  function a " +a);
+ console.log("outside function b" +b);
+    
