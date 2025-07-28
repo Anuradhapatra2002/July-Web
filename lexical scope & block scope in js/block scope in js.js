911b46a98ef867e041f0b321bd399c23bca367a6
Anuradhapatra2002 <patraanuradha09@gmail.com>
2025-07-28 07:24:54 +0530

    block scope

---
+++ lexical scope & block scope in js/block scope in js.js
@@ -0,0 +1,13 @@
+console.log("program started");
+
+let x1=200;
+var y2=313;
+
+if(true){
+    let x2=200;
+    var y2=300;
+}
+
+//console.log(x2); not access outside the block bcz the x2 is declared with in block
+console.log(y2);
+
