df18b4ef68e7cff6388c2c45a98a73266b1a58c7
Anuradhapatra2002 <patraanuradha09@gmail.com>
2025-07-29 07:46:28 +0530

    setTimeOut & setInterval

---
+++ day 16 heigher order function set Timeout setTimeInterval/setTimeout & setInterval.js
@@ -0,0 +1,16 @@
+ console.log(1);
+ setInterval(hello , 5000)
+
+console.log(3);
+ function hello(){
+     console.log("hello everyone")
+    
+ }
+ const id = setTimeout(hello , 1000)
+ console.log(id);
+
+
+console.log(setInterval(hello , 2000));
+
+ 
+
